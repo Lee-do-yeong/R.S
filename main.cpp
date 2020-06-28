3b105f78ea75da65ca93b42b55f2e8464a5cf049
Kim-Gang-Min <63631095+Kim-Gang-Min@users.noreply.github.com>
2020-06-28 21:33:20 +0900

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "poker_header.h"
-
-int main()
-{
-	player member[5] = { {"Player"}, {"Kim"},{"Lee"},{"Bum"},{"IM"} }, dealer; // member[0]가 플레이어
-	playGame(member, dealer);
-
-	return 0;
-}
